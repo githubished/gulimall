337e7620825d9bca731f2868cdb6f4a86e9c0ac7
dhx <1501218997@qq.com>
2023-04-10 11:23:48 +0800

    initialization

---
+++ guliamll-member/src/main/java/com/example/guliamllmember/GuliamllMemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.guliamllmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliamllMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliamllMemberApplication.class, args);
+    }
+
+}
